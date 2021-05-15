e83fc94fa893d6b76d94c2a195a69edfc4032436
HawkDeath <patryj1083@gmail.com>
2021-05-15 12:13:05 +0200

    (unix): renderer_gl compile, todo: refocator

--- src/game/q_math.c
+++ src/game/q_math.c
@@ -646,6 +646,7 @@ int BoxOnPlaneSide2 (vec3_t emins, vec3_t emaxs, struct cplane_s *p)
 
 ==================
 */
+#define __LCC__
 #if !(defined __linux__ && defined __i386__ && !defined C_ONLY)
 #if defined __LCC__ || defined C_ONLY || !id386
 
